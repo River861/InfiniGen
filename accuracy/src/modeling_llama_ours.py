147d285266497589433d41ef8cf4fa255aca4a99
Xuchuan Luo <river861@qq.com>
2025-06-10 22:58:21 +0800

    test

--- accuracy/src/modeling_llama_ours.py
+++ accuracy/src/modeling_llama_ours.py
@@ -251,8 +251,8 @@ class LlamaAttention(nn.Module):
         for i in range(fetch_max, store_max):
             _, ind = torch.topk(attn[:,i, :i+1], k = fetch_num[i], dim = -1)
             fetch_mask[:, i, :i+1] = fetch_mask[:, i, :i + 1].scatter(-1, ind, 1)
-            print(f"!!!!NOTE: i={i} ind={ind} mask={fetch_mask[:, i, :i+1]} count={fetch_mask[:, i, :i+1].float().sum().item} seq_len={i+1}")
-            
+            print(f"debug: i={i} mask={fetch_mask[:, i, :i+1]}")
+            print(f"debug: count={fetch_mask[:, i, :i+1].float().sum().item()} seq_len={i+1} ratio={fetch_mask[:, i, :i+1].float().sum().item() / (i+1)}")
 
         for i in range(store_max, tgt_len):
             _, ind = torch.topk(attn[:,i, :i+1], k = fetch_num[i], dim = -1)
